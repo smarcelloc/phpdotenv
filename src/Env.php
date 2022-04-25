fbdfe4d5abecaa47c55f7af72bb2a9bcc2752997
Marcello Silvério <100056128+smarcelloc@users.noreply.github.com>
2022-04-25 04:22:37 -0300

    add: transform value env to appropriate value type

--- src/Env.php
+++ src/Env.php
@@ -51,10 +51,36 @@ class Env
 
         if (preg_match_all($pattern, $content, $matches)) {
             $keys = array_map('trim', $matches[1]);
-            $values = array_map('trim', $matches[2]);
+            $values = array_map('self::transformToAppropriateValue', $matches[2]);
             $envVariables = array_combine($keys, $values);
         }
 
         return $envVariables;
+    }
+
+    /**
+     * All variables in dotenv files are strings, so some reserved values were 
+     * created to allow you to return an appropriate value type.
+     *
+     * @param string $valueEnv
+     * @return string|bool|null
+     */
+    protected static function transformToAppropriateValue(string $valueEnv)
+    {
+        $valueEnv = trim($valueEnv);
+
+        switch (mb_strtolower($valueEnv)) {
+            case 'true':
+                return true;
+
+            case 'false':
+                return false;
+
+            case 'null':
+                return null;
+
+            default:
+                return trim($valueEnv, '"');
+        }
     }
 }
